85704dd9bb9be558d1b18885a1deb15b77435a93
Carl Woffenden <cwoffenden@gmail.com>
2026-06-23 14:51:16 +0200

    Mostly a tidy (now with BC block initialisers)

--- main.cpp
+++ main.cpp
@@ -63,6 +63,17 @@ struct BCBlock {
 		} bc4;
 		uint64_t data;
 	};
+	BCBlock() : data(0) {}
+	BCBlock(int raw0, int raw1, int raw2, int raw3, int raw4, int raw5, int raw6, int raw7) {
+		raw[0] = raw0;
+		raw[1] = raw1;
+		raw[2] = raw2;
+		raw[3] = raw3;
+		raw[4] = raw4;
+		raw[5] = raw5;
+		raw[6] = raw6;
+		raw[7] = raw7;
+	}
 };
 
 static_assert(sizeof(BCBlock) == 8, "BC block should be 8 bytes");
@@ -81,39 +92,83 @@ static uint32_t u32Bits(float val) {
 	return bits.u;
 }
 
-void dumpBits(const Rf4x4& block, bool newline = true) {
-	printf("Float bits\n0x%08X\n0x%08X\n0x%08X\n0x%08X\n%s",
+void printBits(const Rf4x4& block, bool eight = false, bool newline = true) {
+	puts("floats (bits)");
+	printf("0: 0x%08X\n1: 0x%08X\n2: 0x%08X\n3: 0x%08X\n",
 		u32Bits(block[0][0]),
 		u32Bits(block[0][1]),
 		u32Bits(block[0][2]),
-		u32Bits(block[0][3]), (newline) ? "\n" : "");
+		u32Bits(block[0][3]));
+	if (eight) {
+		printf("4: 0x%08X\n5: 0x%08X\n6: 0x%08X\n7: 0x%08X\n",
+			u32Bits(block[1][0]),
+			u32Bits(block[1][1]),
+			u32Bits(block[1][2]),
+			u32Bits(block[1][3]));
+	}
+	if (newline) {
+		puts("");
+	}
 }
 
-void dump(const Rf4x4& block, bool newline = true) {
-	printf("Floats\n%0.9f\n%0.9f\n%0.9f\n%0.9f\n%s",
+void printVals(const Rf4x4& block, bool eight = false, bool newline = true) {
+	puts("floats");
+	printf("0: %0.8f\n1: %0.8f\n2: %0.8f\n3: %0.8f\n",
 		block[0][0],
 		block[0][1],
 		block[0][2],
-		block[0][3], (newline) ? "\n" : "");
+		block[0][3]);
+	if (eight) {
+		printf("4: %0.8f\n5: %0.8f\n6: %0.8f\n7: %0.8f\n",
+			block[1][0],
+			block[1][1],
+			block[1][2],
+			block[1][3]);
+	}
+	if (newline) {
+		puts("");
+	}
 }
 
-void dump(const Rs4x4& block, bool newline = true) {
-	printf("Shorts\n0x%04X\n0x%04X\n0x%04X\n0x%04X\n%s",
+void printVals(const Rs4x4& block, bool eight = false, bool newline = true) {
+	puts("shorts");
+	printf("0: 0x%04X\n1: 0x%04X\n2: 0x%04X\n3: 0x%04X\n",
 		block[0][0],
 		block[0][1],
 		block[0][2],
-		block[0][3], (newline) ? "\n" : "");
+		block[0][3]);
+	if (eight) {
+		printf("4: 0x%04X\n5: 0x%04X\n6: 0x%04X\n7: 0x%04X\n",
+			block[1][0],
+			block[1][1],
+			block[1][2],
+			block[1][3]);
+	}
+	if (newline) {
+		puts("");
+	}
 }
 
-void dump(const Rb4x4& block, bool newline = true) {
-	printf("Bytes\n0x%02X\n0x%02X\n0x%02X\n0x%02X\n%s",
+void printVals(const Rb4x4& block, bool eight = false, bool newline = true) {
+	puts("bytes");
+	printf("0: 0x%02X\n1: 0x%02X\n2: 0x%02X\n3: 0x%02X\n",
 		block[0][0],
 		block[0][1],
 		block[0][2],
-		block[0][3], (newline) ? "\n" : "");
+		block[0][3]);
+	if (eight) {
+		printf("4: 0x%02X\n5: 0x%02X\n6: 0x%02X\n7: 0x%02X\n",
+			block[1][0],
+			block[1][1],
+			block[1][2],
+			block[1][3]);
+	}
+	if (newline) {
+		puts("");
+	}
 }
 
-void dumpBoundRedChannel() {
+void dumpBoundRedChannel(bool eight = false) {
 	Rf4x4 f32blk;
 	glGetTexImage(GL_TEXTURE_2D, 0, GL_RED, GL_FLOAT, f32blk);
 	Rs4x4 u16blk;
@@ -123,53 +178,44 @@ void dumpBoundRedChannel() {
 	GLenum err = glGetError();
 	assert(err == 0);
 
-	dumpBits(f32blk);
-	dump(f32blk);
-	dump(u16blk);
-	dump(u08blk);
+	printBits(f32blk, eight);
+	printVals(f32blk, eight);
+	printVals(u16blk, eight);
+	printVals(u08blk, eight);
 }
 
 void bc3RedTest() {
-	BCBlock block[2] = {};
-	// block[0] is the alpha
+	BCBlock block[2] = {{/*unused alpha*/}, {0x00, 0x00, 0x00, 0x00, 0xE4, 0xE4, 0xE4, 0xE4}};
 	block[1].bc1.endpt[0].r = 31;
-	block[1].bc1.endpt[1].r = 0;
-	block[1].bc1.texels = 0xEE44EE44;
+	block[1].bc1.endpt[1].r =  8;
+
 	GLuint txName = 0;
 	glGenTextures(1, &txName);
 	glBindTexture(GL_TEXTURE_2D, txName);
 	glCompressedTexImage2D(GL_TEXTURE_2D, 0, GL_COMPRESSED_RGBA_S3TC_DXT3_EXT, 4, 4, 0, 16, block);
 
-	dumpBoundRedChannel();
+	dumpBoundRedChannel(false);
 	
 	glDeleteTextures(1, &txName);
 }
 
 void bc4RedTest() {
-	BCBlock block[1] = {};
-	block[0].bc4.endpt[0] = 0xFF;
-	block[0].bc4.endpt[1] = 0x00;
-	block[0].bc4.y0x0 = 0;
-	block[0].bc4.y0x1 = 1;
-	block[0].bc4.y0x2 = 2;
-	block[0].bc4.y0x3 = 3;
-	block[0].bc4.y1x0 = 4;
-	block[0].bc4.y1x1 = 5;
-	block[0].bc4.y1x2 = 6;
-	block[0].bc4.y1x3 = 7;
-	
+	BCBlock block[1] = {{0x00, 0x00, 0x88, 0xC6, 0xFA, 0x88, 0xC6, 0xFA}};
+	block[0].bc4.endpt[0] = 255;
+	block[0].bc4.endpt[1] =  64;
+
 	GLuint txName = 0;
 	glGenTextures(1, &txName);
 	glBindTexture(GL_TEXTURE_2D, txName);
 	glCompressedTexImage2D(GL_TEXTURE_2D, 0, GL_COMPRESSED_RED_RGTC1, 4, 4, 0, 8, block);
 	
-	dumpBoundRedChannel();
+	dumpBoundRedChannel(true);
 	
 	glDeleteTextures(1, &txName);
 }
 
 void setup() {
-	//bc3RedTest();
+	bc3RedTest();
 	bc4RedTest();
 }
 
